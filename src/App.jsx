2254e8727d586c08b8e974c15429508e655f719c
Lukas <lukyn.odehnal333@gamil.com>
2021-07-09 14:52:29 +0200

    delte request

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 
 import './App.css';
-import axios from "axios";
-import { useState } from 'react';
 import List from "./components/List"
 import Input from './components/Input';
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,19 +5,26 @@ function Input() {
     const [Name, setName] = useState("")
 
     async function set(e){
-        
         e.preventDefault()
         const res = await axios.post("https://jsonplaceholder.typicode.com/users", {Name})
         console.log(res);
-        console.log(res.data);
-        
+        console.log(res.data); 
+    }
+
+    async function deleteR(e){
+        e.preventDefault()
+        const res = await axios.delete(`https://jsonplaceholder.typicode.com/users/${Name}`, {Name})
+        console.log(res);
+        console.log(res.data); 
     }
 
 
     return (
         <div>
+            <p>Type in name of input you want to add or if of input you want to delete</p>
             <input name="Name" value={Name} onChange={e => setName(e.target.value)} type="text" />
             <button onClick={e => set(e)}>Input</button>
+            <button onClick={e => deleteR(e)}>Delete</button>
         </div>
     )
 }
